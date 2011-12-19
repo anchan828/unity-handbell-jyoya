e19fac32d9853f2448d1a955873a237421c189a4
keigo.ando <anchan828@gmail.com>
2011-12-19 21:51:21 +0900

    commit

---
+++ Assets/Acceleration.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Specialized;
+public class Acceleration : MonoBehaviour
+{
+
+	private Vector3 accele;
+	private float oldAccle;
+	private float num;
+	private float abs = 0;
+	private bool flag = false;
+	public AudioClip sound;
+	void Start ()
+	{
+		num = 0.7f;
+		oldAccle = 0.7f;
+		abs = 0;
+		flag = false;
+	}
+	void FixedUpdate ()
+	{
+		accele = Input.acceleration;
+		abs = Mathf.Abs (accele.magnitude - oldAccle);
+		print ("abs : " + abs);
+		if (abs > num && !flag) {
+			print ("call");
+			audio.PlayOneShot (sound);
+			StartCoroutine (Wait ());
+		}
+		oldAccle = accele.magnitude;
+	}
+	IEnumerator Wait ()
+	{
+		flag = true;
+		yield return new WaitForSeconds (7);
+		flag = false;
+	}
+}
